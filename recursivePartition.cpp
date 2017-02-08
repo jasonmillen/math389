6d033d88cca0a91369cbdde5fc81efe196d905b7
Jason Millen <millen@umich.edu>
2017-02-08 17:24:14 -0500

    updating recursive solution to try to store values across runs of the program

--- recursivePartition.cpp
+++ recursivePartition.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
-void print2dVec(vector< vector<int> > & v) {
-	cout << "size: " << v.size() << endl;
-	return;
+vector< vector<int> > part5 = { { 5 }, { 4, 1 }, { 3, 2 }, { 3, 1, 1 }, { 2, 2, 1 }, { 2, 1, 1, 1 }, { 1, 1, 1, 1, 1 } };
 
+void printPartitonSize(int num, vector< vector<int> > & partitions) {
+	cout << "num: " << num << ", size: " << partitions.size() << endl;
+}
+
+void print2dVec(vector< vector<int> > & v) {
 	for(size_t i = 0; i < v.size(); i++) {
 		for(size_t j = 0; j < v[i].size(); j++) {
 			cout << v[i][j] << " ";
@@ -14,6 +18,36 @@ void print2dVec(vector< vector<int> > & v) {
 		cout << endl;
 	}
 	cout << endl;
+}
+
+void print2dVecToStore(vector< vector<int> > & v) {
+
+	cout << "{ ";
+
+	for(size_t i = 0; i < v.size() - 1; i++) {
+		cout << "{ ";
+		for(size_t j = 0; j < v[i].size() - 1; j++) {
+			cout << v[i][j] << ", ";
+		}
+		cout << v[i][v[i].size() - 1];
+		cout << " }, ";
+	}
+
+	// last partition
+	cout << "{ ";
+	for(size_t i = 0; i < v[v.size() - 1].size() - 1; i++) {
+		cout << v[v.size() - 1][i] << ", ";
+	}
+	cout << v[v.size() - 1][v[v.size() - 1].size() - 1];
+	cout << " }";
+	cout << " }";
+	cout << endl;
+}
+
+void writePartitionsToFile(vector< vector<int> > & partitions) {
+	ofstream outputFile("partition.txt");
+
+
 }
 
 vector< vector<int> > nextPartition(vector< vector<int> > & curPartition) {
@@ -58,31 +92,40 @@ vector< vector<int> > nextPartition(vector< vector<int> > & curPartition) {
 
 }
 
+vector< vector<int> > sixtyPartitions;
+
 
 
 int main() {
 
 	vector< vector<int> > partitions;
 
-	vector<int> v;
-	v.push_back(2);
-	partitions.push_back(v);
-	v.clear();
+	// initialize with partition for 2
+	// vector<int> v;
+	// v.push_back(2);
+	// partitions.push_back(v);
+	// v.clear();
 
-	v.push_back(1);
-	v.push_back(1);
-	partitions.push_back(v);
-
-	//print2dVec(partitions);
+	// v.push_back(1);
+	// v.push_back(1);
+	// partitions.push_back(v);
 
 
-	for(int i = 0; i < 60; i++) {
+	partitions = part5;
+
+	for(int i = 6; i <= 30; i++) {
 		partitions = nextPartition(partitions);
-		print2dVec(partitions);
+		printPartitonSize(i, partitions);
 	}
 
+	print2dVecToStore(partitions);
 
 
+	// vector< vector<int> > blah = { {1, 2, 3}, {2, 4} };
+
+	// print2dVec(blah);
+
+	
 }
 
 
