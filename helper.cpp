03df83edf84d7eb5e70feddf43949173b8a986d0
Millen <millen@umich.edu>
2017-02-14 00:34:09 -0500

    working on data and growth rates

--- helper.cpp
+++ helper.cpp
@@ -29,10 +29,30 @@ void twoToTheN1ToN(int n) {
 	cout << pow(2, n) << endl;
 	
 }
+
+double factorial(int n) {
+	
+	double sol = 1;
+
+	for(int i = n; i >= 1; i--) {
+		sol *= i;
+	}
+	return sol;
+	
+}
+
+void factorial1ToN(int n) {
+	for(int i = 1; i < n; i++) {
+		cout << factorial(i) << ", ";
+	}
+	cout << factorial(n) << endl;
+}
+	
 	
 
 int main() {
 
+	factorial1ToN(100);
 	twoToTheN1ToN(100);
 
 }
