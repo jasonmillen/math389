b0062f67a6ffd46625427002ee8c21c3e6bc681a
Jason Millen <millen@umich.edu>
2017-03-08 14:32:29 -0500

    more work

--- Project2/FixedPoint.cpp
+++ Project2/FixedPoint.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 // use str.find() to see if string is contained in another string
+
+vector<string> rules = {"0", "1", "00", "01", "10", "11", "000", "001", "010", "011", "100", "101", "110", "111"};
+vector<string> seeds = {"0", "1", "00", "01", "10", "11", "000", "001", "010", "011", "100", "101", "110", "111"};
+
+void printRules(int size, string cur) {
+	if(cur.size() == size) {
+		cout << cur << endl;
+	}
+	else {
+		printRules(size, cur + "0");
+		printRules(size, cur + "1");
+	}
+
+}
 
 string generateNewSeq(unordered_map<char, string> & rule, string & cur) {
 	string newSeq;
@@ -47,22 +62,51 @@ void printSequences(unordered_map<char, string> & rule, string seed, int numIter
 	}
 }
 
+void findFixedPoints() {
+	unordered_map<char, string> rule;
+	string seed;
+
+	for(int i = 0; i < rules.size(); i++) {
+		for(int j = 0; j < rules.size(); j++) {
+			for(int k = 0; k < seeds.size(); k++) {
+
+				rule['0'] = rules[i];
+				rule['1'] = rules[j];
+				seed = seeds[k];
+
+				if(testFixedPoint(rule, seed, 15)) {
+					cout << "0 -> " << rule['0'] << endl;
+					cout << "1 -> " << rule['1'] << endl;
+					cout << "seed: " << seed << endl;
+					cout << "--------" << endl;
+				}
+
+			}
+		}
+	}
+}
+
 
 int main() {
+
+	findFixedPoints();
+	return 0;
+
+	printRules(3, "");
 	
 	unordered_map<char, string> rule;
 	rule['0'] = "01";
 	rule['1'] = "10";
 
-	string seed = "0";
+	string seed = "1";
 
 
-	printSequences(rule, seed, 30);
-	// if(testFixedPoint(rule, seed, 100)) {
-	// 	cout << "yes" << endl;
-	// }
-	// else {
-	// 	cout << "no" << endl;
-	// }
+	printSequences(rule, seed, 15);
+	if(testFixedPoint(rule, seed, 15)) {
+		cout << "yes" << endl;
+	}
+	else {
+		cout << "no" << endl;
+	}
 
 }
